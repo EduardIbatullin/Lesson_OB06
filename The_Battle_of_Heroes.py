51212a67d5fb93e5459719ed68a60c107b304919
EduardIbatullin <e.ibatullin@gmail.com>
2024-05-24 23:43:50 +0500

    cteate cod game The battle of Heroes

--- The_Battle_of_Heroes.py
+++ The_Battle_of_Heroes.py
@@ -1,4 +1,3 @@
-# from abc import ABC, abstractmethod
 import random
 
 
@@ -9,13 +8,41 @@ class Hero:
         self.attack_power = attack_power
 
     def attack(self, character):
-        self.attack_power += self.attack_power * random.randint(-10, 10)
+        self.attack_power += random.randint(-5, 5)
         character.health -= self.attack_power
         print(f'{self.name} атаковал {character.name}, и нанёс {self.attack_power} очков урона.')
-        print(f'У {character.name} осталось {character.health} очков здоровья.')
+        if character.health >= 0:
+            print(f'У {character.name} осталось {character.health} очков здоровья.')
+        else:
+            print(f'{character.name} умер.')
 
     def is_alive(self):
         return self.health > 0
 
 
+class Game:
+    def __init__(self, hero1, hero2):
+        self.hero1 = hero1
+        self.hero2 = hero2
 
+    def start(self):
+        round_number = 1
+        while self.hero1.is_alive() and self.hero2.is_alive():
+            print(f"\nРаунд {round_number}.")
+            self.hero1.attack(self.hero2)
+            if not self.hero2.is_alive():
+                break
+            self.hero2.attack(self.hero1)
+            round_number += 1
+        if self.hero1.is_alive():
+            print(f'\n{self.hero1.name} выиграл!')
+        else:
+            print(f'\n{self.hero2.name} выиграл!')
+
+
+hero_1 = Hero('Игрок', 100, 20)
+hero_2 = Hero('Компьютер', 100, 20)
+
+game = Game(hero_1, hero_2)
+
+game.start()
